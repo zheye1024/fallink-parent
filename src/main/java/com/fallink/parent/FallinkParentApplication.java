090acf2ebb6f8d46d12f40f36564cc0c3bfed4f7
zheye <zheye@yiji.com>
2018-11-29 20:56:55 +0800

    first commit

---
+++ src/main/java/com/fallink/parent/FallinkParentApplication.java
@@ -0,0 +1,12 @@
+package com.fallink.parent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FallinkParentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FallinkParentApplication.class, args);
+    }
+}
